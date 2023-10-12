e915f48144b48621a4ad4d427e74e58c5a81fdd1
EsneiderUni <146643826+EsneiderUni@users.noreply.github.com>
2023-10-11 20:16:21 -0500

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,6 +5,5 @@ module.exports = {
   USER: process.env.USER,
   PASSWORD: process.env.PASSWORD,
   DB: process.env.DB,
-  DB_PORT: process.env.DB_PORT, 
-  PORT: process.env.PORT
+  DB_PORT: process.env.DB_PORT
 };
